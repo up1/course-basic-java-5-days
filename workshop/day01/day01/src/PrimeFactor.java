5b4b14ba115d48e59a32a423b8f00e30e7a3411f
up1 <somkiat.p@gmail.com>
2022-07-18 14:45:18 +0700

    Workshop 01 :: Prime factor

---
+++ workshop/day01/day01/src/PrimeFactor.java
@@ -0,0 +1,13 @@
+public class PrimeFactor {
+    public static void main(String[] args) {
+        PrimeFactor p = new PrimeFactor();
+        String result = p.print(2);
+        System.out.println(result);
+        result = p.print(4);
+        System.out.println(result);
+    }
+
+    private String print(int input) {
+        return "2";
+    }
+}
