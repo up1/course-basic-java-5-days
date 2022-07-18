502c34e82b1cffd4689caf63d3c89c04b1c124fd
up1 <somkiat.p@gmail.com>
2022-07-18 15:15:52 +0700

    Working with 2

--- workshop/day01/day01/src/PrimeFactor.java
+++ workshop/day01/day01/src/PrimeFactor.java
@@ -5,9 +5,22 @@ public class PrimeFactor {
         System.out.println(result);
         result = p.print(4);
         System.out.println(result);
+        result = p.print(8);
+        System.out.println(result);
+        result = p.print(16);
+        System.out.println(result);
     }
+//    8
+//    8/2 = 4  => 2
+//    4/2 = 2  => 2
+//    2/2 = 1  => 2
 
     private String print(int input) {
-        return "2";
+        String result = "";
+        while (input%2 == 0) {
+            result = result + "2";
+            input = input/2;
+        }
+        return result;
     }
 }
