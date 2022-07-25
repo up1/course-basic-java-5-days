a6fa6602b7c4c55b4dd0541836d10b2cbab0a3e4
up1 <somkiat.p@gmail.com>
2022-07-25 10:45:52 +0700

    Working with class

---
+++ workshop/day02/src/Marathon2.java
@@ -0,0 +1,11 @@
+public class Marathon2 {
+
+    public static void main(String[] args) {
+        Runner[] runners = new Runner[5];
+        runners[0] = new Runner("Elena", 341);
+        runners[1] = new Runner("Thomas", 273);
+        runners[2] = new Runner("Hamilton", 278);
+        runners[3] = new Runner("Emma", 275);
+        runners[4] = new Runner("John", 243);
+    }
+}
---
+++ workshop/day02/src/Runner.java
@@ -0,0 +1,12 @@
+class Runner {
+    // Properties
+    String name;
+    int runningTimeInMinute;
+
+    public Runner(String name, int runningTimeInMinute) {
+        this.name = name;
+        this.runningTimeInMinute = runningTimeInMinute;
+    }
+
+    // Methods/ behaviors
+}
