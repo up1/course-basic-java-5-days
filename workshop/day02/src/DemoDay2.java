6427ab9ff290d71249bb9e9ecfd77627743114d6
up1 <somkiat.p@gmail.com>
2022-07-25 09:50:01 +0700

    Workshop day 2

---
+++ workshop/day02/src/DemoDay2.java
@@ -0,0 +1,9 @@
+public class DemoDay2 {
+    public static void main(String[] args) {
+        String[] names = new String[5];
+        names[1] = "Somkiat";
+        for (int i = 0; i < names.length; i++) {
+            System.out.println(names[i]);
+        }
+    }
+}
---
+++ workshop/day02/src/Marathon.java
@@ -0,0 +1,8 @@
+public class Marathon {
+
+    public static void main(String[] args) {
+        String[] runnerNames = new String[10];
+        int[] runningTimes = new int[10];
+        // TODO :: xyz
+    }
+}
