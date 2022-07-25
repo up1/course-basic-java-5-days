d8a653fcb27144413dbdfc142fc3641d9293ce72
up1 <somkiat.p@gmail.com>
2022-07-25 10:53:22 +0700

    Change from array to list

--- workshop/day02/src/Marathon2.java
+++ workshop/day02/src/Marathon2.java
@@ -1,11 +1,14 @@
+import java.util.ArrayList;
+import java.util.List;
+
 public class Marathon2 {
 
     public static void main(String[] args) {
-        Runner[] runners = new Runner[5];
-        runners[0] = new Runner("Elena", 341);
-        runners[1] = new Runner("Thomas", 273);
-        runners[2] = new Runner("Hamilton", 278);
-        runners[3] = new Runner("Emma", 275);
-        runners[4] = new Runner("John", 243);
+        List<Runner> runners = new ArrayList<>();
+        runners.add(new Runner("Elena", 341));
+        runners.add(new Runner("Thomas", 273));
+        runners.add(new Runner("Hamilton", 278));
+        runners.add(new Runner("Emma", 275));
+        runners.add(new Runner("John", 243));
     }
 }
