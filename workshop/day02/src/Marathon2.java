19c61a58de9a30ba1f28d71143d86ad3af117990
up1 <somkiat.p@gmail.com>
2022-07-25 11:13:45 +0700

    Working with arraylist

--- workshop/day02/src/Marathon2.java
+++ workshop/day02/src/Marathon2.java
@@ -10,5 +10,15 @@ public class Marathon2 {
         runners.add(new Runner("Hamilton", 278));
         runners.add(new Runner("Emma", 275));
         runners.add(new Runner("John", 243));
+
+        int minTime = runners.get(0).runningTimeInMinute;
+        int position = 0;
+        for (int i = 0; i < runners.size(); i++) {
+            if (minTime > runners.get(i).runningTimeInMinute) {
+                minTime = runners.get(i).runningTimeInMinute;
+                position = i;
+            }
+        }
+        System.out.println(runners.get(position).name);
     }
 }
