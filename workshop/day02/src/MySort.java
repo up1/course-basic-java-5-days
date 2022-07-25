ccaca6f5c2699188844f74fc0cf9038d2776cd7b
up1 <somkiat.p@gmail.com>
2022-07-25 13:58:11 +0700

    Completed with case 03

--- workshop/day02/src/MySort.java
+++ workshop/day02/src/MySort.java
@@ -1,7 +1,16 @@
+import java.util.ArrayList;
 import java.util.List;
 
 public class MySort {
     public List<Integer> sort(List<Integer> input) {
-        return input;
+        List<Integer> result = new ArrayList<>();
+        if((input.size() > 1) && (input.get(0) > input.get(1))) {
+            // Swap
+            result.add(input.get(1));
+            result.add(input.get(0));
+        } else {
+            result = input;
+        }
+        return result;
     }
 }
