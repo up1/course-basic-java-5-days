6446a6f2c50fd26de977d1b55a421eaf9f9ff89a
up1 <somkiat.p@gmail.com>
2022-07-25 10:13:40 +0700

    Workshop with array and for loop

--- workshop/day02/src/Marathon.java
+++ workshop/day02/src/Marathon.java
@@ -2,7 +2,21 @@ public class Marathon {
 
     public static void main(String[] args) {
         String[] runnerNames = new String[10];
-        int[] runningTimes = new int[10];
-        // TODO :: xyz
+        runnerNames[0] = "Elena";
+        runnerNames[1] = "Thomas";
+        runnerNames[2] = "Hamilton";
+        runnerNames[3] = "Emma";
+        runnerNames[4] = "John";
+        int[] runningTimes = {341, 273, 278, 275, 243};
+
+        int minTime = runningTimes[0];
+        int position = 0;
+        for (int i = 1; i < runningTimes.length; i++) {
+            if (minTime > runningTimes[i]) {
+                minTime = runningTimes[i];
+                position = i;
+            }
+        }
+        System.out.println(position + "=" + runnerNames[position]);
     }
 }
