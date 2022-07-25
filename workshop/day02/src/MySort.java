df1ee8c2ccb2157a0c096f89a79355d2af00aba9
up1 <somkiat.p@gmail.com>
2022-07-25 15:41:57 +0700

    Add Comparator

--- workshop/day02/src/MySort.java
+++ workshop/day02/src/MySort.java
@@ -1,26 +1,29 @@
-import java.util.ArrayList;
-import java.util.List;
+import java.util.*;
 
+class MySortComparator implements Comparator<Integer> {
+    @Override
+    public int compare(Integer o1, Integer o2) {
+        return -1;
+    }
+}
 public class MySort {
     public List<Integer> sort(List<Integer> input) {
-            /*
-            [1, 3], 2 => 1, 3, 2
-            1, [3, 2] => 1, 2, 3
-             */
-        for (int i = 0; i < input.size() - 1; i++) {
-            if (input.get(i) > input.get(i + 1)) {
-                // Swap
-                int temp = input.get(i);
-                input.set(i, input.get(i + 1));
-                input.set(i + 1, temp);
-            }
-        }
+        Collections.sort(input);
+        Collections.sort(input, new MySortComparator());
+//        for (int i = 0; i < input.size() - 1; i++) {
+//            if (input.get(i) > input.get(i + 1)) {
+//                // Swap
+//                int temp = input.get(i);
+//                input.set(i, input.get(i + 1));
+//                input.set(i + 1, temp);
+//            }
+//        }
         return input;
     }
 
     public static void main(String[] args) {
-        if(false)
+        if (false)
             System.out.println("Line 1");
-            System.out.println("Line 2");
+        System.out.println("Line 2");
     }
 }
