115cabe437506de300487073c86cac1ae1165ebe
up1 <somkiat.p@gmail.com>
2022-07-27 13:49:17 +0700

    Demo with order + send email

---
+++ workshop/day03/src/demo/EmailService.java
@@ -0,0 +1,8 @@
+package demo;
+
+public class EmailService {
+    public boolean sendEmail(String targetEmail,
+                             String subject, String body) {
+        return false;
+    }
+}
---
+++ workshop/day03/src/demo/Order.java
@@ -0,0 +1,20 @@
+package demo;
+
+final class Constants {
+    public static final int ORDER_STATUS_COMPLETED = 1;
+    public static final int ORDER_STATUS_WAITING = 2;
+    public static final int ORDER_STATUS_DELIVERED = 3;
+    public static final int ORDER_STATUS_CANCELED = 4;
+}
+
+enum OrderStatus {
+    Completed,
+    Waiting,
+    Delivered,
+    Canceled
+}
+
+public class Order {
+    String customerEmail;
+    OrderStatus orderStatus;
+}
