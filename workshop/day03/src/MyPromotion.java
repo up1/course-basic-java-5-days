e061aad1179f1906774149e45f03dcce21780fdb
up1 <somkiat.p@gmail.com>
2022-07-27 09:36:25 +0700

    First promotion

---
+++ workshop/day03/src/MyPromotion.java
@@ -0,0 +1,30 @@
+public class MyPromotion {
+    public static void main(String[] args) {
+        int input = 1;
+        process(input);
+    }
+
+    private static void process(int input) {
+        if(input == 1) {
+            Promotion1 p = new Promotion1();
+            p.calculateDiscount();
+        } else if(input == 2) {
+            Promotion2 p = new Promotion2();
+            p.calculateDiscount();
+        }
+    }
+}
+
+class Promotion1 {
+    int calculateDiscount() {
+        return 100;
+    }
+}
+
+class Promotion2 {
+    int calculateDiscount() {
+        return 200;
+    }
+}
+
+
