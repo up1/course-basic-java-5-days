8ca6334638427f54983b2b27d39c677e68cc1868
up1 <somkiat.p@gmail.com>
2022-07-27 14:38:58 +0700

    Add more status

--- workshop/day03/src/demo/OrderMgt.java
+++ workshop/day03/src/demo/OrderMgt.java
@@ -3,7 +3,7 @@ package demo;
 public class OrderMgt {
     public static void main(String[] args) {
         Order order1 = new Order();
-        order1.orderStatus = OrderStatus.Completed;
+        order1.orderStatus = OrderStatus.Waiting;
         order1.customerEmail = "xxx@xxx.com";
 
         String subject = "";
@@ -11,6 +11,17 @@ public class OrderMgt {
         if(order1.orderStatus == OrderStatus.Completed) {
             subject = "Subject for completed";
             body = "Body for completed";
+        } else if(order1.orderStatus == OrderStatus.Waiting) {
+            subject = "Subject for waiting";
+            body = "Body for waiting";
+        } else if(order1.orderStatus == OrderStatus.Delivered) {
+            subject = "Subject for delivered";
+            body = "Body for delivered";
+        } else if(order1.orderStatus == OrderStatus.Canceled) {
+            subject = "Subject for cancel";
+            body = "Body for cancel";
+        } else {
+            // Not support
         }
         EmailService service = new EmailService();
         EmailRequest request = new EmailRequest(
