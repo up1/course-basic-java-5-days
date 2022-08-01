18b7494ba2dd0a11fa35dbae50c66ce0fde9e6dd
up1 <somkiat.p@gmail.com>
2022-08-01 14:03:37 +0700

    Working with parameterized test

--- workshop/day04/src/CalculatorTest.java
+++ workshop/day04/src/CalculatorTest.java
@@ -1,4 +1,6 @@
 import org.junit.jupiter.api.Test;
+import org.junit.jupiter.params.ParameterizedTest;
+import org.junit.jupiter.params.provider.CsvSource;
 
 import static org.junit.jupiter.api.Assertions.*;
 
@@ -9,6 +11,20 @@ class CalculatorTest {
         Calculator calculator = new Calculator();
         double result = calculator.divide(8,2);
         assertEquals(4, result);
+    }
+
+    @ParameterizedTest
+    @CsvSource( {
+            "8, 2, 4",
+            "4, 2, 2",
+            "2, 1, 2"
+    })
+    public void success_case02(int a,
+                               int b,
+                               double expected) {
+        Calculator calculator = new Calculator();
+        double result = calculator.divide(a, b);
+        assertEquals(expected, result);
     }
 
 }
