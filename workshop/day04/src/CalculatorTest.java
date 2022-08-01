815fd9d44a63990232db6291e984fadafc83e116
up1 <somkiat.p@gmail.com>
2022-08-01 14:17:55 +0700

    Working with assert throws

--- workshop/day04/src/CalculatorTest.java
+++ workshop/day04/src/CalculatorTest.java
@@ -1,10 +1,22 @@
 import org.junit.jupiter.api.Test;
+import org.junit.jupiter.api.function.Executable;
 import org.junit.jupiter.params.ParameterizedTest;
 import org.junit.jupiter.params.provider.CsvSource;
 
 import static org.junit.jupiter.api.Assertions.*;
 
 class CalculatorTest {
+
+    @Test
+    public void fail_case() {
+        Calculator calculator = new Calculator();
+        assertThrows(
+                CalculatorDivideWithZeroException.class,
+                () ->  {
+                    calculator.divide(8, 0) ;
+                });
+    }
+
 
     @Test
     public void success_case() {
